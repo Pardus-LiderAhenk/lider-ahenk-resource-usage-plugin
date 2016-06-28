7fe3ebfb49d716d4d5bee76d39289c13bcea0e66
cemrealpsoy <cemrealpsoy@gmail.com>
2016-06-28 14:30:38 +0300

    Logs are rearranged

--- ahenk-resource-usage/resource-usage/resource_info_fetcher.py
+++ ahenk-resource-usage/resource-usage/resource_info_fetcher.py
@@ -14,31 +14,37 @@ class ResourceUsage(AbstractPlugin):
         self.context = context
         self.logger = self.get_logger()
         self.message_code = self.get_message_code()
-        self.MAX_NET_USAGE = 400000
-        self.MAX_ATTACKS = 4
 
     def handle_task(self):
+        self.logger.debug("Handling Resource Info Fetcher Task")
         device = ""
-        for part in System.Hardware.Disk.partitions():
-            if len(device) != 0:
-                device += ", "
-            device = device + part.device
-        data = {'System': System.Os.name(), 'Release': System.Os.kernel_release(),
-                'Version': System.Os.distribution_version(), 'Machine': System.Os.architecture(),
-                'CPU Physical Core Count': System.Hardware.Cpu.physical_core_count(),
-                'Total Memory': System.Hardware.Memory.total(),
-                'Usage': System.Hardware.Memory.used(),
-                'Total Disc': System.Hardware.Disk.total(),
-                'Usage Disc': System.Hardware.Disk.used(),
-                'Processor': System.Hardware.Cpu.brand(),
-                'Device': device,
-                'CPU Logical Core Count': System.Hardware.Cpu.logical_core_count(),
-                'CPU Actual Hz':  System.Hardware.Cpu.hz_actual(),
-                'CPU Advertised Hz': System.Hardware.Cpu.hz_advertised()
-                }
-
-        self.context.create_response(code=self.message_code.TASK_PROCESSED.value, message='resource-usage-response',
-                                     data=data, content_type=ContentType.APPLICATION_JSON.value)
+        try:
+            for part in System.Hardware.Disk.partitions():
+                if len(device) != 0:
+                    device += ", "
+                device = device + part.device
+            data = {'System': System.Os.name(), 'Release': System.Os.kernel_release(),
+                    'Version': System.Os.distribution_version(), 'Machine': System.Os.architecture(),
+                    'CPU Physical Core Count': System.Hardware.Cpu.physical_core_count(),
+                    'Total Memory': System.Hardware.Memory.total(),
+                    'Usage': System.Hardware.Memory.used(),
+                    'Total Disc': System.Hardware.Disk.total(),
+                    'Usage Disc': System.Hardware.Disk.used(),
+                    'Processor': System.Hardware.Cpu.brand(),
+                    'Device': device,
+                    'CPU Logical Core Count': System.Hardware.Cpu.logical_core_count(),
+                    'CPU Actual Hz':  System.Hardware.Cpu.hz_actual(),
+                    'CPU Advertised Hz': System.Hardware.Cpu.hz_advertised()
+                    }
+            self.logger.debug("System Info is ready to send")
+            self.context.create_response(code=self.message_code.TASK_PROCESSED.value, message='resource-usage-response',
+                                         data=data, content_type=ContentType.APPLICATION_JSON.value)
+            self.logger.debug("Resource Info Fetcher Task processed successfully")
+        except Exception as e:
+            self.logger.debug(str(e))
+            self.context.create_response(code=self.message_code.TASK_ERROR.value,
+                                         message='Error in Resource Info Fetcher Task',
+                                         content_type=ContentType.APPLICATION_JSON.value)
 
 
 def handle_task(task, context):
