ebd071376752c3819051c2a39c191961f3d9ba0c
emrekgn <emre.akkaya@agem.com.tr>
2016-07-25 17:58:26 +0300

    cep impl

--- ahenk-resource-usage/resource-usage/resource_info_alert.py
+++ ahenk-resource-usage/resource-usage/resource_info_alert.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Author: Cemre ALPSOY <cemre.alpsoy@agem.com.tr>
+# Author: Emre Akkaya <emre.akkaya@agem.com.tr>
 
 from base.plugin.abstract_plugin import AbstractPlugin
-from base.system.system import System
 from base.model.enum.ContentType import ContentType
+from threading import Thread
+import time
+import psutil
 
 
 class ResourceUsage(AbstractPlugin):
@@ -14,52 +17,56 @@ class ResourceUsage(AbstractPlugin):
         self.context = context
         self.logger = self.get_logger()
         self.message_code = self.get_message_code()
+        self.is_running = False
+        self._threadCep = None
 
     def handle_task(self):
+        try:
+            action = self.data['action']
+            self.logger.debug("[RESOURCE USAGE] Action: {0}".format(action))
 
-        result = []
-        items = self.data['items']
-        for item in items:
-            if (str(item['type']) == 'Memory' or str(
-                    item['type']) == 'Bellek') and System.Hardware.Memory.percent() > float(item['limit']):
-                if str(item['type']) == 'Memory':
-                    result.append("Memory Usage has gone over the limit value .\r\nLimit Value: %" + item[
-                        'limit'] + "\r\nCalculated Value: %" + str(
-                        System.Hardware.Memory.percent()) + "\r\nAhenk Ip Adresses:" + str(
-                        System.Hardware.Network.ip_addresses()) + "\r\nAhenk Mac Adresses:" + str(
-                        System.Hardware.Network.mac_addresses()))
-                else:
-                    result.append("Memory Kullanımı Sınır Değeri Aşıldı.\r\nLimit Değeri: %" + item[
-                        'limit'] + "\r\nHesaplanılan Değer: %" + str(
-                        System.Hardware.Memory.percent()) + "\r\nAhenk Ip Adresleri:" + str(
-                        System.Hardware.Network.ip_addresses()) + "\r\nAhenk Mac Adresleri:" + str(
-                        System.Hardware.Network.mac_addresses()))
-                result.append(item['email'])
-            if str(item['type']) == 'Disc' and float(item['limit']) < System.Hardware.Disk.percent():
-                if str(item['type']) == 'Disc':
-                    result.append("Disk Usage has gone over the limit value.\r\nLimit Value: %" + item[
-                    'limit'] + "\r\nCalculated Value: %" + str(
-                    System.Hardware.Disk.percent()) + "\r\nAhenk Ip Adresses:" + str(
-                    System.Hardware.Network.ip_addresses()) + "\r\nAhenk Mac Adresses:" + str(
-                    System.Hardware.Network.mac_addresses()))
-                else:
-                    result.append("Disk Kullanımı Sınır Değeri Aşıldı.\r\nLimit Değeri: %" + item[
-                    'limit'] + "\r\nHesaplanılan Değer: %" + str(
-                    System.Hardware.Disk.percent()) + "\r\nAhenk Ip Adresleri:" + str(
-                    System.Hardware.Network.ip_addresses()) + "\r\nAhenk Mac Adresleri:" + str(
-                    System.Hardware.Network.mac_addresses()))
-                result.append(item['email'])
-                # if str(item['type']) == 'CPU' and float(item['limit']) < System.Hardware.Cpu.cpu_percent()
-                #     result.append("CPU Kullanımı Sınır Değeri Aşıldı.\r\nLimit Değeri: %"+item['limit']+"\r\nHesaplanılan Değer: %"+str(System.Hardware.Cpu.cpu_percent())+"\r\nAhenk Ip Adresses:"+str(System.Hardware.Network.ip_addresses())+"\r\nAhenk Mac Adresses:"+str(System.Hardware.Network.mac_addresses()))
-                #     result.append(item['email'])
-        data = {'Result': result}
-        self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
-                                     message='resource-usage-alert-response',
-                                     data=data, content_type=ContentType.APPLICATION_JSON.value)
+            if action == "start_timer":
+                self.gather_resource_usage()
+                self.is_running = True
+                self._threadCep = Thread(target=self.run_timer,
+                                         args=(int(self.data['interval']), self.context.get('task_id')))
+                self._threadCep.start()
+                self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                             message='Kaynak kullanım bilgileri toplanmaya başlandı.',
+                                             content_type=ContentType.APPLICATION_JSON.value)
+            elif action == "stop_timer":
+                self.is_running = False
+                self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                             message='Kaynak kullanım bilgilerinin toplanması durduruldu.',
+                                             content_type=ContentType.APPLICATION_JSON.value)
+        except Exception as e:
+            self.logger.error(str(e))
+            self.context.create_response(code=self.message_code.TASK_ERROR.value,
+                                         message='Kaynak kullanım bilgileri işlenirken hata oluştu: ' + str(e),
+                                         content_type=ContentType.APPLICATION_JSON.value)
+
+    def run_timer(self, interval, task_id):
+        while self.is_running:
+            self.gather_resource_usage(task_id)
+            time.sleep(interval)
+
+    def gather_resource_usage(self, task_id):
+        # Memory usage
+        memory_usage = psutil.virtual_memory()
+        self.logger.debug("[RESOURCE USAGE] Memory usage: {0}".format(memory_usage))
+        # Disk usage
+        disk_usage = psutil.disk_usage('/')
+        self.logger.debug("[RESOURCE USAGE] Disk usage: {0}".format(disk_usage))
+        # CPU usage
+        cpu_percentage = psutil.cpu_percent(interval=1, percpu=True)
+        self.logger.debug("[RESOURCE USAGE] CPU percentage: {0}".format(cpu_percentage))
+
+        command = 'python3 /opt/ahenk/ahenkd.py send -t {0} -m "{{ \'memoryUsage\': {1}, \'diskUsage\': {2}, \'cpuPercentage\': {3} }}"'.format(task_id, memory_usage, disk_usage, cpu_percentage)
+        result_code, p_out, p_err = self.execute(command)
+        if result_code != 0:
+            self.logger.error("[PACKAGE MANAGER] Error occurred while sending message: " + str(p_err))
 
 
 def handle_task(task, context):
-    print('RESOURCE USAGE ALERT TASK')
     plugin = ResourceUsage(task, context)
     plugin.handle_task()
-    print('RESOURCE USAGE ALERT TASK HANDLED')
